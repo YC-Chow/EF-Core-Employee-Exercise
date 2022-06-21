ec7c562e87739714acbdde632d47622a0f79500d
Chow Yun Cong <S10194206@connect.np.edu.sg>
2022-06-21 15:02:36 +0800

    Add DateTImeConverter class

---
+++ EmployeeEx/DateTimeConverterUsingDateTimeParse.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System;
+
+namespace EmployeeEx {
+    public class DateTimeConverterUsingDateTimeParse : JsonConverter<DateTime> {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+            Debug.Assert(typeToConvert == typeof(DateTime));
+            return DateTime.Parse(reader.GetString());
+        }
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
