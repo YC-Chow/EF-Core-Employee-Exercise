127d643751104c3a1a8137520dc5800061f78b99
Chow Yun Cong <S10194206@connect.np.edu.sg>
2022-06-29 15:14:10 +0800

    Add/Modify Entity base class

--- EFDataAccessLibrary/Models/EmployeeFolder/EmployeeAddressNoIndex.cs
+++ EFDataAccessLibrary/Models/EmployeeFolder/EmployeeAddressNoIndex.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFDataAccessLibrary.Models.EmployeeFolder {
-    public class EmployeeAddress {
+    public class EmployeeAddressNoIndex {
         public int Id { get; set; }
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string Address3 { get; set; }
         public int ZipCode { get; set; }
         public int EmployeeId { get; set; }
-        public virtual  Employee Employee { get; set; }
-
+        public virtual EmployeeNoIndex Employee { get; set; }
     }
 }
--- EFDataAccessLibrary/Models/EmployeeFolder/EmployeeNoIndex.cs
+++ EFDataAccessLibrary/Models/EmployeeFolder/EmployeeNoIndex.cs
@@ -3,18 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using EFDataAccessLibrary.Models.CompanyFolder;
 
 namespace EFDataAccessLibrary.Models.EmployeeFolder {
-    public class Employee {
+    public class EmployeeNoIndex {
         public int Id { get; set; }
         public string FName { get; set; }
         public string MName { get; set; }
         public string LName { get; set; }
         public DateTime CreatedDate { get; set; }
-        public virtual List<EmployeeAddress> Addresses { get; set; }
-
+        public virtual List<EmployeeAddressNoIndex> Addresses { get; set; }
     }
 }
