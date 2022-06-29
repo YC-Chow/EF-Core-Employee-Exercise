411645eeb3c76283fa868f82c7082f881353bc83
Chow Yun Cong <S10194206@connect.np.edu.sg>
2022-06-29 15:14:46 +0800

    Add new benchmarks

---
+++ EmployeeEx/BenchMarks/OrderByNoIndexBenchmarks.cs
@@ -0,0 +1,130 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models.EmployeeFolder;
+using System.Linq;
+
+namespace EmployeeEx.BenchMarks {
+    [MemoryDiagnoser]
+    [MaxIterationCount(150)]
+    [MinIterationCount(100)]
+    [Config(typeof(Config))]
+    
+    public class OrderByNoIndexBenchmarks {
+
+        public class Config : ManualConfig {
+            public Config() {
+                this.SummaryStyle.WithTimeUnit(Perfolizer.Horology.TimeUnit.Millisecond);
+            }
+        }
+        [Benchmark]
+        public void OrderByCreatedDateNoIndexDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex
+                    .OrderByDescending(emp => emp.CreatedDate)
+                    .Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .ToList();
+            }
+        }
+        [Benchmark]
+        public void OrderByCreatedDateNoIndexApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex.Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .ToList()
+                .OrderByDescending(emp => emp.CreatedDate);
+            }
+        }
+        [Benchmark]
+        public void OrderByCreatedDateNoIndexWithTakeDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex
+                    .OrderByDescending(emp => emp.CreatedDate)
+                    .Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .Take(5)
+                .ToList();
+            }
+        }
+
+        [Benchmark]
+        public void OrderByCreatedDateNoIndexWithTakeApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex.Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .ToList()
+                .OrderByDescending(emp => emp.CreatedDate)
+                .Take(5);
+            }
+        }
+        [Benchmark]
+        public void OrderByFnameNoIndexDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex.Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .OrderByDescending(emp => emp.FName)
+                .ToList();
+            }
+        }
+
+        [Benchmark]
+        public void OrderByFnameNoIndexApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex.Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .ToList()
+                .OrderByDescending(emp => emp.FName);
+            }
+        }
+
+        [Benchmark]
+        public void OrderByFnameNoIndexWithTakeDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex.Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .OrderByDescending(emp => emp.FName)
+                .Take(5)
+                .ToList();
+            }
+        }
+
+        [Benchmark]
+        public void OrderByFnameNoIndexWithTakeApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.EmployeeNoIndex.Select(emp => new Employee() {
+                    FName = emp.FName,
+                    MName = emp.MName,
+                    LName = emp.LName
+                })
+                .ToList()
+                .OrderByDescending(emp => emp.FName)
+                .Take(5);
+            }
+        }
+
+       
+    }
+}
---
+++ EmployeeEx/BenchMarks/OrderByWithIndexBenchmarks.cs
@@ -0,0 +1,134 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models.EmployeeFolder;
+using System.Linq;
+
+namespace EmployeeEx.BenchMarks {
+    [MemoryDiagnoser]
+    [MinIterationCount(100)]
+    [MaxIterationCount(200)]
+    
+
+    public class OrderByWithIndexBenchmarks {
+
+      
+        [Benchmark]
+        public void OrderByCreatedDateWithIndexDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee
+                    .OrderByDescending(p => p.CreatedDate)
+                    .Select(p => new Employee() {
+                        FName = p.FName,
+                        MName = p.MName,
+                        LName = p.LName,
+                        CreatedDate = p.CreatedDate
+                    })
+                    .ToList();
+            }
+        }
+
+        [Benchmark]
+        public void OrderByCreatedDateWithIndexApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee.Select(p => new Employee() {
+                    FName = p.FName,
+                    MName = p.MName,
+                    LName = p.LName,
+                    CreatedDate = p.CreatedDate
+                })
+                .ToList()
+                .OrderByDescending(p => p.CreatedDate);
+            }
+        }
+
+        [Benchmark]
+        public void OrderByCreatedDateWithIndexWithTakeDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee
+                    .OrderByDescending(p => p.CreatedDate)
+                    .Select(p => new Employee() {
+                        FName = p.FName,
+                        MName = p.MName,
+                        LName = p.LName,
+                        CreatedDate = p.CreatedDate
+                    })
+                    .Take(5)
+                    .ToList();
+            }
+        }
+
+        [Benchmark]
+        public void OrderByCreatedDateWithIndexWithTakeApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee.Select(p => new Employee() {
+                    FName = p.FName,
+                    MName = p.MName,
+                    LName = p.LName,
+                    CreatedDate = p.CreatedDate
+                })
+                .ToList()
+                .OrderByDescending(p => p.CreatedDate)
+                .Take(5);
+            }
+        }
+
+        [Benchmark]
+        public void OrderByFNameWithIndexDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee.Select(p => new Employee() {
+                    FName = p.FName,
+                    MName = p.MName,
+                    LName = p.LName,
+                    CreatedDate = p.CreatedDate
+                })
+                .OrderByDescending(p => p.FName)
+                .ToList();
+            }
+        }
+
+        [Benchmark]
+        public void OrderByFNameWithIndexApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee.Select(p => new Employee() {
+                    FName = p.FName,
+                    MName = p.MName,
+                    LName = p.LName,
+                    CreatedDate = p.CreatedDate
+                })
+                .ToList()
+                .OrderByDescending(p => p.FName);
+            }
+        }
+
+        [Benchmark]
+        public void OrderByFNameWithIndexWithTakeDB() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee.Select(p => new Employee() {
+                    FName = p.FName,
+                    MName = p.MName,
+                    LName = p.LName,
+                    CreatedDate = p.CreatedDate
+                })
+                .OrderByDescending(p => p.FName)
+                .Take(5)
+                .ToList();
+            }
+        }
+
+        [Benchmark]
+        public void OrderByFNameWithIndexWithTakeApp() {
+            using (var _db = new EmployeeContext()) {
+                _db.Employee.Select(p => new Employee() {
+                    FName = p.FName,
+                    MName = p.MName,
+                    LName = p.LName,
+                    CreatedDate = p.CreatedDate
+                })
+                .ToList()
+                .OrderByDescending(p => p.FName)
+                .Take(5);
+            }
+        }
+    }
+}
