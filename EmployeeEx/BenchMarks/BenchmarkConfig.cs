7d479e7f928a5d171c64c37ea20fb201b8ebf3bc
Chow Yun Cong <S10194206@connect.np.edu.sg>
2022-06-29 15:14:37 +0800

    Add BenchmarkConfig Class

---
+++ EmployeeEx/BenchMarks/BenchmarkConfig.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters.Csv;
+using Perfolizer.Horology;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Loggers;
+
+namespace EmployeeEx.BenchMarks {
+    public class BenchmarkConfig : ManualConfig {
+        public BenchmarkConfig() {
+            AddColumnProvider(DefaultColumnProviders.Instance);
+            AddLogger(new ConsoleLogger());
+            AddDiagnoser(MemoryDiagnoser.Default);
+            AddExporter(new CsvExporter(
+                CsvSeparator.CurrentCulture,
+                new SummaryStyle(
+                    cultureInfo: System.Globalization.CultureInfo.CurrentCulture,
+                    printUnitsInHeader: false,
+                    printUnitsInContent: true,
+                    timeUnit: TimeUnit.Millisecond,
+                    sizeUnit: SizeUnit.KB
+                )));
+            this.SummaryStyle = SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond).WithSizeUnit(SizeUnit.KB);
+        }
+    }
+}
