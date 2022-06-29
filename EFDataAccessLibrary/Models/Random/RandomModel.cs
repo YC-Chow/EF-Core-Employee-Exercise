293037821b31ebeb57fb327b4f4f51c0d769c4b0
Chow Yun Cong <S10194206@connect.np.edu.sg>
2022-06-29 15:14:25 +0800

    Add RandomModel

---
+++ EFDataAccessLibrary/Models/Random/RandomModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Models.Random {
+    public class RandomModel {
+        public int Id { get; set; }
+        public int Column1 { get; set; }
+        public int Column2 { get; set; }
+        public int Column3 { get; set; }
+        public int Column4 { get; set; }
+        public int Column5 { get; set; }
+        public int Column6 { get; set; }
+        public int Column7 { get; set; }
+        public int Column8 { get; set; }
+        public int Column9 { get; set; }
+        public int Column10 { get; set; }
+        public int Column11 { get; set; }
+        public int Column12 { get; set; }
+        public int Column13 { get; set; }
+        public int Column14 { get; set; }
+        public int Column15 { get; set; }
+        public int Column16 { get; set; }
+        public int Column17 { get; set; }
+        public int Column18 { get; set; }
+        public int Column19 { get; set; }
+        public int Column20 { get; set; }
+        public int Column21 { get; set; }
+        public int Column22 { get; set; }
+        public int Column23 { get; set; }
+        public int Column24 { get; set; }
+        public int Column25 { get; set; }
+        public int Column26 { get; set; }
+        public int Column27 { get; set; }
+        public int Column28 { get; set; }
+        public int Column29 { get; set; }
+        public int Column30 { get; set; }
+        public int Column31 { get; set; }
+        public int Column32 { get; set; }
+        public int Column33 { get; set; }
+        public int Column34 { get; set; }
+        public int Column35 { get; set; }
+        public int Column36 { get; set; }
+        public int Column37 { get; set; }
+        public int Column38 { get; set; }
+        public int Column39 { get; set; }
+        public int Column40 { get; set; }
+        public int Column41 { get; set; }
+        public int Column42 { get; set; }
+        public int Column43 { get; set; }
+        public int Column44 { get; set; }
+        public int Column45 { get; set; }
+        public int Column46 { get; set; }
+        public int Column47 { get; set; }
+        public int Column48 { get; set; }
+        public int Column49 { get; set; }
+        public int Column50 { get; set; }
+    }
+}
